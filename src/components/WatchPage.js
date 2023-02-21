1005096924191fa8d7bfdc413550de4583cd3dce
Ankit Khowal <ankitkhowal@Ankits-MacBook-Air.local>
2023-02-21 19:42:03 +0530

    live feature

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { closeMenu } from "../utils/appSlice";
 import CommentsContainer from './CommentsContainer';
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
     const dispatch = useDispatch();
@@ -16,17 +17,23 @@ const WatchPage = () => {
         dispatch(closeMenu());
     }, []);
     return (
-        <div className="flex flex-col">
-            <div className="px-5">
-                <iframe
-                    width="1200"
-                    height="600"
-                    src={"https://www.youtube.com/embed/" + searchParams.get("v") + "?autoplay=1"}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen>
-                </iframe>
+        <div className="flex flex-col w-full">
+            <div className="px-5 flex">
+                <div>
+                    <iframe
+                        width="1200"
+                        height="600"
+                        src={"https://www.youtube.com/embed/" + searchParams.get("v") + "?autoplay=1"}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen>
+                    </iframe>
+                </div>
+                <div className="w-full">
+                    <LiveChat />
+                </div>
+                
             </div>
             <CommentsContainer />
         </div>
